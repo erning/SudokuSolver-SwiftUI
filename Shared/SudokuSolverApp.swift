9974f9489c751e06d62b0a8060a3cb95c5d2e61d
Zhang Erning <zhang@erning.com>
2021-01-18 17:33:34 +0800

    Initial Commit

---
+++ Shared/SudokuSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolverApp.swift
+//  Shared
+//
+//  Created by Zhang Erning on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
